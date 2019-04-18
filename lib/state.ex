ef118405dccc207a561d067be1972411476452b8
qubbit <gopala@email.arizona.edu>
2019-04-17 22:02:16 -0400

    Keep track of line and column in state

--- lib/state.ex
+++ lib/state.ex
@@ -3,35 +3,42 @@ defmodule State do
   Module to facilitate traversing and maintaining state of the input string
   """
 
-  defstruct [:string, :offset]
+  defstruct [:string, :offset, :line, :column]
 
   @doc """
   Create and return a new `State` with the input string and an offset of 0
   """
   def new(string) do
-    %State{string: string, offset: 0}
+    %State{string: string, offset: 0, line: 1, column: 0}
   end
 
   @doc """
   Peek up to `offset + n` positions from the current offset and return that
   string slice
   """
-  def peek(%State{offset: offset, string: string}, n) do
-    String.slice(string, offset, n)
+  def peek(%State{offset: o, string: s}, n) do
+    String.slice(s, o, n)
   end
 
   @doc """
   Advance the offset by `n` positions. This means we have consumed up to `n +
   offset` position of the input string
   """
-  def read(%State{offset: offset} = state, n) do
-    %{state | offset: offset + n}
+  def read(%State{offset: o, line: l, column: c} = state, n) do
+    lines = state
+            |> peek(n)
+            |> String.split(~r/\R/)
+
+    line_count = Enum.count(lines) - 1
+    column_count = Enum.at(lines, -1) |> String.length
+
+    %{state | offset: o + n, line: l + line_count, column: c + column_count}
   end
 
   @doc """
   Returns true if we have finished consuming the input string
   """
-  def complete?(%State{offset: offset, string: string}) do
-    offset == String.length(string)
+  def complete?(%State{offset: o, string: s}) do
+    o >= String.length(s)
   end
 end
